b49a185c11e7c1d5ce1818fcde296f0f156a45e4
V. Naydov <vencislavnajdov@gmail.com>
2023-02-19 15:40:07 +0200

    Update random_sentences_generator_by_vencislav.py

--- random_sentences_generator_by_vencislav.py
+++ random_sentences_generator_by_vencislav.py
@@ -21,5 +21,5 @@ while True:
     random_noun = get_random_word(nouns)
     random_adverb = get_random_word(adverbs)
     random_detail = get_random_word(details)
-    print(f'{random_name} from {random_place} {random_adverb} {random_verb} {random_noun}')
+    print(f'{random_name} from {random_place} {random_adverb} {random_verb} {random_noun} {random_detail}')
     input('Click [Enter] to generate a new one.')
